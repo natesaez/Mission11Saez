d34acab2c50c92016355489d933b655219da6e1d
natesaez <167953576+natesaez@users.noreply.github.com>
2026-03-22 20:05:18 -0600

    Database connected

---
+++ backend/Mission11Saez/Mission11Saez/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11Saez.data;
+
+namespace Mission11Saez.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        public IEnumerable<Book> GetBooks()
+        {
+            var something = _bookContext.Books.ToList();
+            return something;
+        }
+    } 
+}
+
