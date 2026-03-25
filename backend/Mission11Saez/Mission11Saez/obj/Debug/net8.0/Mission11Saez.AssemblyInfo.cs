3a8b0902dbdc5f1015f7f8ffb5c52970d2a41707
natesaez <167953576+natesaez@users.noreply.github.com>
2026-03-24 20:38:57 -0600

    delete weather

--- backend/Mission11Saez/Mission11Saez/obj/Debug/net8.0/Mission11Saez.AssemblyInfo.cs
+++ backend/Mission11Saez/Mission11Saez/obj/Debug/net8.0/Mission11Saez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11Saez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1beace3d76d9e0b2f5d564c3d67ee55854d1d310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3421168baed840c1dbfa90cefc95a2eb356635b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11Saez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11Saez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
